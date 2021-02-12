b5342a3688787cc442fd2ff4e31ee5f872506d4a
ZindGH <zzzRRzzz@mail.ru>
2021-02-12 20:58:58 +0300

    Windows adopted version

--- image_correction.py
+++ image_correction.py
@@ -1,23 +1,22 @@
 import os
 from PIL import Image
 
-proj_folder = os.getcwd() + '/images/'
-pic_full_path, names, pic_num = [], [], 0
+proj_folder = '~/supplier-data/images/'
+pic_full_path, names, = [], []
 
 for path in os.listdir(proj_folder):
-    if '48dp' in path:
-        pic_num += 1
+    if '.tiff' in path:
         pic_full_path.append(proj_folder + path)
-        names.append(path)
+        names.append(path.replace('.tiff', '.jpeg'))
 
 
 def imageProcess(pic_dir, path_tosave, name):
     pic = Image.open(pic_dir)
     pic = pic.rotate(90).resize((128, 128)).convert('RGB')
-    pic.save(path_tosave + name, 'jpeg')
+    pic.save(path_tosave + name, )
     return
 
 
 if __name__ == '__main__':
     for (pic_dir, name) in zip(pic_full_path, names):
-        imageProcess(pic_dir, '/opt/icons/', name)
+        imageProcess(pic_dir, proj_folder, name)
